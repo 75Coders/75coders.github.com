8895c173a66012b154b09a4555c7b22d94b85557
Brooks Robinson <pyrogoggles@gmail.com>
2014-03-31 09:40:01 -0400

    Create index.js

---
+++ page/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("div").fadeOut({
+      
+    });
+  });
+  
+  $("button").click(function(){
+    $("div").fadeIn({});
+  });
+});
